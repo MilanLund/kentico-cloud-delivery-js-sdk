f02ff44f3e9132b876cd0e069bfbda3467070624
Martin Cajthaml <martin.cajthaml@symbio.agency>
2017-11-23 11:50:00 +0100

    update main script

--- __tests__/index.js
+++ __tests__/index.js
@@ -13,7 +13,7 @@ describe('basics', () => {
     const project = new Delivery('6380f5f9-4bd0-4e80-91f5-9da2ccf3bd6b', 'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAidWlkIjogInVzcl8wdlZLT3BNazlrRFBTcThnYUZRQnlmIiwNCiAgImVtYWlsIjogImxlQHN5bWJpby5hZ2VuY3kiLA0KICAicHJvamVjdF9pZCI6ICI2MzgwZjVmOS00YmQwLTRlODAtOTFmNS05ZGEyY2NmM2JkNmIiLA0KICAianRpIjogImdFbUR6WWpON3dnTm1mTFAiLA0KICAidmVyIjogIjEuMC4wIiwNCiAgImdpdmVuX25hbWUiOiAiTEVPIEV4cHJlc3MiLA0KICAiZmFtaWx5X25hbWUiOiAiU1lNQklPIiwNCiAgImF1ZCI6ICJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSINCn0.cHMA5bijursuRVAMet_liMYFXZWQYhJfD4MZh5RlSro')
     const options = {}
 
-    options[type] = '?system.type=' + type + '&depth=0';
+    options[type] = '?system.type=' + type + '&depth=0'
 
     const data = await project.getContent(options, true)
 
